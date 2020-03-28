e65c5263fe329d71f6c7cb29b71b260fab612ee5
remarkies <luka@ichbinich.ch>
2020-03-28 15:33:08 +0100

    canvas cleanup

--- public/processor.js
+++ public/processor.js
@@ -11,28 +11,47 @@ let processor = {
     },
 
     doLoad: function() {
-        this.video = document.getElementById("remoteVideo");
-        this.c1 = document.getElementById("remoteCanvas");
-        this.ctx1 = this.c1.getContext("2d");
+        var canvas = document.getElementById('remoteCanvas');
+        var ctx = canvas.getContext('2d');
+        var video = document.getElementById('remoteVideo');
         let self = this;
+        // set canvas size = video size when known
+        video.addEventListener('loadedmetadata', function() {
+            let ratio = video.videoWidth / video.videoHeight;
+            this.width = canvas.width = window.innerWidth;
+            this.height = canvas.height = window.innerWidth / ratio;
 
-        this.video.addEventListener("play", function() {
-            self.width = self.video.offsetWidth / 12;
-            self.height = self.video.offsetHeight / 10;
-            self.timerCallback();
-        }, false);
+            canvas.setAttribute("style", "top: " + (window.innerHeight - canvas.height) / 2 + "px; display: flex;");
+            video.setAttribute("style", "top: " + (window.innerHeight - canvas.height) / 2 + "px; display: flex;");
+        });
+
+        video.addEventListener('play', function() {
+            var $this = this; //cache
+            (function loop() {
+                if (!$this.paused && !$this.ended) {
+
+                    ctx.drawImage($this, 0, 0, canvas.width, canvas.height);
+
+                    self.computeFrame();
+
+                    setTimeout(loop, 1000 / 16); // drawing at 60fps
+                }
+            })();
+        }, 0);
     },
 
     computeFrame: function() {
 
-        if(this.width > 0 && this.height > 0) {
-            this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
+        let canvas = document.getElementById('remoteCanvas');
 
-            var frame = this.ctx1.getImageData(0, 0, this.width, this.height);
+        if(canvas.width > 0 && canvas.height > 0) {
 
-            var l = frame.data.length / 4;
+            let ctx = canvas.getContext('2d');
 
-            console.log()
+            let frame = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+            let l = frame.data.length / 4;
+
             for (var i = 0; i < l; i++) {
                 var grey = (frame.data[i * 4 + 0] + frame.data[i * 4 + 1] + frame.data[i * 4 + 2]) / 3;
 
@@ -40,7 +59,9 @@ let processor = {
                 frame.data[i * 4 + 1] = grey;
                 frame.data[i * 4 + 2] = grey;
             }
-            this.ctx1.putImageData(frame, 0, 0);
+
+
+            ctx.putImageData(frame, 0, 0);
         }
 
         return;
